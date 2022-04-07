8b9c6394f417b6ec917edc206e00dad23c8e8123
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-04-07 16:36:45 +0630

    Initial config

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container justify-content-between">
+      <router-link to="/" class="navbar-brand">
+        <img src="../assets/logo.png" alt="Logo">
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link :to="{name:'Home'}" :class="['nav-link',{'active':$route.name==='Home'}]">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name:'About'}" :class="['nav-link',{'active':$route.name==='About'}]">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name:'Database'}" :class="['nav-link',{'active':$route.name==='Database'}]">Database
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  .navbar-brand {
+    img {
+      width: 100%;
+      height: 40px;
+    }
+  }
+}
+</style>
