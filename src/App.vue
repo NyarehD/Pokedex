e7905f21eb5027c5b9fbb986fab367bea38d40f2
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-04-07 16:36:15 +0630

    Init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <div class="container-md">
+    <Navbar></Navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-4">
+      <img src="../assets/logo.png" alt="">
+    </div>
+  </div>
+<h1 class="text-center">This is about page</h1>
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
