4f012219d1dcff27d0b5bfe9ca934b5ddc2456c3
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-04-09 19:41:43 +0630

    Fixed navbar not collapsing after routing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,21 +5,24 @@
         <img src="../assets/logo.png" alt="Logo">
       </router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+              :class="{'collapsed':navbarCollapseButton}">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <div class="navbar-collapse justify-content-end"
+           :class="{'collapse':navbarCollapseDiv,'show':navbarShow,'collapsing':navbarCollapsing}"
+           id="navbarSupportedContent">
         <ul class="navbar-nav mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link :to="{name:'Home'}" :class="['nav-link',{'active':$route.name==='Home'}]">Home</router-link>
+            <a @click.prevent="toggleNavbarStatus('Home')"
+               :class="['nav-link',{'active':$route.name==='Home'}]">Home</a>
           </li>
           <li class="nav-item">
-            <router-link :to="{name:'About'}" :class="['nav-link',{'active':$route.name==='About'}]">About</router-link>
+            <a @click.prevent="toggleNavbarStatus('About')"
+               :class="['nav-link',{'active':$route.name==='About'}]">About</a>
           </li>
           <li class="nav-item">
-            <router-link :to="{name:'Pokedex'}" :class="['nav-link',{'active':$route.name==='Pokedex'}]">
-              Pokedex
-            </router-link>
+            <a @click.prevent="toggleNavbarStatus('Pokedex')" :class="['nav-link',{'active':$route.name==='Pokedex'}]">Pokedex</a>
           </li>
         </ul>
       </div>
@@ -28,6 +31,29 @@
 </template>
 
 <script setup>
+import {useRouter} from "vue-router"
+import {ref} from "vue";
+
+let router = useRouter();
+
+const navbarCollapseButton = ref(true);
+const navbarCollapseDiv = ref(true)
+const navbarCollapsing = ref(false);
+const navbarShow = ref(false);
+
+function toggleNavbarStatus(to) {
+  navbarCollapseButton.value = false
+  navbarCollapseDiv.value = false
+  navbarCollapsing.value = true
+  navbarShow.value = true
+  setTimeout(() => {
+    navbarCollapseButton.value = true
+    navbarCollapseDiv.value = true
+    navbarCollapsing.value = false
+    navbarShow.value = false
+  }, 300)
+  router.push({name: to})
+}
 </script>
 
 <style lang="scss" scoped>
