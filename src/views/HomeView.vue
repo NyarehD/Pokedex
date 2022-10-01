d3543d45b90dac94815225c977203a2259c9c23b
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-10-01 09:24:51 +0630

    Added: Infinite scrolling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,32 @@
-<script setup>
-  import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main class="container mx-auto">
-    <TheWelcome/>
-    <TheWelcome/>
+  <main class="customContainer min-h-screen">
+    <div
+      class="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4"
+      ref="cardContainer">
+      <Card
+        v-for="pokemon in pokemonStore.pokemonList"
+        :pokemon="pokemon"
+        :key="pokemon.name" />
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+  import Card from "@/components/Card.vue";
+  import { usePokemonStore } from "@/stores/pokemon";
+  import { onBeforeMount, onMounted, ref } from "vue";
+  import { useInfiniteScroll } from "@vueuse/core";
+
+  const cardContainer = ref<HTMLHtmlElement>();
+  const pokemonStore = usePokemonStore();
+
+  onBeforeMount(() => {
+    pokemonStore.fetchPokemonApi("https://pokeapi.co/api/v2/pokemon?limit=20");
+  });
+  onMounted(() => {
+    useInfiniteScroll(window, () => {
+      console.log(pokemonStore.nextLink);
+      pokemonStore.fetchPokemonApi(pokemonStore.nextLink);
+    });
+  });
+</script>
