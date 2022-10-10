f440868216c5ad20d0415a6bec190e2b5d5fdc32
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-10-10 11:50:06 +0630

    Fixed: Nav Link not active in light mode

--- src/components/NavBarLink.vue
+++ src/components/NavBarLink.vue
@@ -1,9 +1,11 @@
 <template v-once>
   <button
-    class="relative text-gray-500 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200 font-medium before:absolute before:-bottom-1 before:h-0.5 before:w-full before:scale-x-0 before:bg-gray-800 dark:before:hover:bg-neutral-200 before:transition hover:before:scale-x-100 text-lg font-medium"
+    class="relative font-medium before:absolute before:-bottom-1 before:h-0.5 before:w-full before:scale-x-0 before:bg-gray-800 dark:before:hover:bg-neutral-200 before:transition hover:before:scale-x-100 text-lg font-medium"
     :class="{
       'text-blue-800 dark:text-cyan-400 hover:text-blue-800 dark:hover:text-cyan-400 before:bg-blue-800 dark:before:hover:bg-cyan-400':
         $route.name === props.routeName,
+      'text-gray-500 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200 dark:text-neutral-400 dark:hover:text-neutral-200':
+        $route.name !== props.routeName,
     }"
     @click.prevent="goToRoute"
     @click="$emit('toggleNavBar')">
@@ -12,7 +14,6 @@
 </template>
 
 <script setup lang="ts">
-  import { defineProps } from "vue";
   import router from "@/router";
 
   function goToRoute() {
