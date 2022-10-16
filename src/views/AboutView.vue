78e1c6aa9739dd5636cec1360600613e50ce87ed
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-10-16 12:49:47 +0630

    Added: contents in AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,17 @@
-<template>This is about</template>
+<template>
+  <div class="customContainer">
+    <h1 class="text-center text-4xl font-bold mb-4">
+      This is a sample project
+    </h1>
+    <h2 class="text-center text-3xl font-bold text-red-500 mb-2">
+      The logo used here is not mine
+    </h2>
+    <p class="text-center text-xl mb-2">This project is to test my skills</p>
+    <p class="text-center text-xl mb-2">
+      Tech stacks: Vue, Tailwind, SCSS(Not many but still)
+    </p>
+    <p class="text-center text-xl mb-2">Libraries: VueUse, Pinia, Vue-Router</p>
+  </div>
+</template>
 <script setup></script>
 <style></style>
