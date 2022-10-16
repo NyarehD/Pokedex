0b11b7e9a77e67ebba061a95139f16f015cd43e5
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-10-16 12:39:18 +0630

    Fixed: LoadingEvolutionChart not responsive in small device

--- src/components/loading/LoadingEvolutionChart.vue
+++ src/components/loading/LoadingEvolutionChart.vue
@@ -3,7 +3,7 @@
     <h2 class="text-3xl font-semibold dark:text-neutral-200 text-center mb-6">
       Evolution Chain
     </h2>
-    <div class="grid grid-cols-3 gap-4">
+    <div class="grid md:grid-cols-3 gap-4">
       <div
         class="w-full h-72 object-contain drop-shadow-none animate-pulse bg-neutral-400/50 dark:bg-neutral-100/50 rounded-t-md"></div>
       <div
